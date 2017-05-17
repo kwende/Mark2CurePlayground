cbaa3a6b69ec3ad86ff02d1c05a3c5a0e43b2b4e
Ben <ben@ben-rush.net>
2017-05-17 13:12:20 -0500

    Turns out Google search isn't free. Have to find a different route.

--- Mark2CurePlayground/GoogleSearch.py
+++ Mark2CurePlayground/GoogleSearch.py
@@ -1,15 +1,31 @@
 from googleapiclient.discovery import build
 import pprint
+import json
+import time
 
 my_api_key = "AIzaSyBBFHOoNxvZrFt2rw14S4jzpVGISN5K05o"
 my_cse_id = "008276682434845921014:uj522xx4rcc"
 
-def google_search(search_term, api_key, cse_id, **kwargs):
-    service = build("customsearch", "v1", developerKey=api_key)
-    res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
-    return res['items']
+lines = open('c:/users/brush/desktop/notfound.txt').readlines()
 
-results = google_search(
-    'weakness of distal muscles', my_api_key, my_cse_id, num=10)
-for result in results:
-    pprint.pprint(result)
+with open('c:/users/brush/desktop/recommendations.txt', 'w+') as fout:
+    def google_search(search_term, api_key, cse_id, **kwargs):
+        service = build("customsearch", "v1", developerKey=api_key)
+        res = service.cse().list(q=search_term, cx=cse_id, **kwargs).execute()
+        return res['items']
+
+    for line in lines:
+        results = google_search(
+            line, my_api_key, my_cse_id, num=10)
+
+        fout.write(line + '\n')
+        for result in results:
+            title = result["title"]
+            snippet = result["snippet"]
+
+            fout.write('\t' + title + '\n')
+            fout.write('\t' + snippet + '\n')
+            fout.write('\n')
+
+        print('sleeping...\n')
+        time.sleep(1)
