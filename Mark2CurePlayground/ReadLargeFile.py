09f3b704704a2eb5c74bf17715833f5088e0fa3a
Ben <ben@ben-rush.net>
2017-05-21 23:13:28 -0500

    Still thinking.

--- Mark2CurePlayground/ReadLargeFile.py
+++ Mark2CurePlayground/ReadLargeFile.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python3
 import mmap
 
-with open('C:/Users/Ben/Desktop/BioNLP/desc2017.xml', 'rb', 0) as file, \
+with open('C:/Users/Ben/Desktop/BioNLP/supp2017.xml', 'rb', 0) as file, \
      mmap.mmap(file.fileno(), 0, access=mmap.ACCESS_READ) as s:
-    i = s.find(b'CDG') #do another find, loop over all
-    if i != -1:
-        part = s[i-20:i+20]
-        print(part)
+    i = 0
+    while True:
+        i = s.find(b'ALG1', i+1) #do another find, loop over all
+        if i != -1:
+            part = s[i-20:i+20]
+            print(part)
+        else:
+            break
